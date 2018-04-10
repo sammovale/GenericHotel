e68e7414b57ba52c4647caa2f13b01ae0d1f4cc3
Sam Vale <sammovale@hotmail.com>
2018-04-10 15:11:20 +0930

    Moved the map javascript function into the separate js file

---
+++ hotel.js
@@ -0,0 +1,13 @@
+function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru,
+           gestureHandling: 'cooperative' //to prevent the page scrolling when using two finger scroll on laptops etc
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+}
+
