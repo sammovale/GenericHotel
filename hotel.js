0dd6aff7a7dd8335340e43dffc6689e6012c68ee
Sam Vale <sammovale@hotmail.com>
2018-04-11 01:09:31 +0930

    Added a slim map
    Added a slim map into the hotel view page to show the specific location of the hotel

--- hotel.js
+++ hotel.js
@@ -11,3 +11,16 @@ function initMap() {
         });
 }
 
+function initMap2() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map2'), {
+          zoom: 4,
+          center: uluru,
+           gestureHandling: 'cooperative' //to prevent the page scrolling when using two finger scroll on laptops etc
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+}
+
