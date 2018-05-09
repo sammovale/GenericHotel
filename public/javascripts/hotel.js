04031f891ae6c9ffdcbe72cc4559e448c1089626
Sam Vale <sammovale@hotmail.com>
2018-05-09 12:00:00 +0930

    Started adding hotels.json
    Started adding the data structure in.
    Need to work on using AJAX so when search.html loads it uses a get request with the search parameters to get the correct hotels from the database

--- public/javascripts/hotel.js
+++ public/javascripts/hotel.js
@@ -24,8 +24,6 @@ $(document).ready(function(){
     addRoom(roomStruct);
   });
 
-  $("#indexSearchBar").click(function(){
-  });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var hotels = [];
+
+fs.readFile('data/hotels.json', 'utf8', function(err, data){ 
+    hotels = JSON.parse(data);
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/search.html', function(req, res){
+router.get('/search', function(req, res){
   //search and return the data for the search site
+  var searchQuery = req.query.search;
+  res.redirect('/search.html?search=' + searchQuery);
 });
 
 
