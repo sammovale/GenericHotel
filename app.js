bada8d345ce095458577d480a13284226ddd7f74
DavidHo123 <a4b11a4b11a@gmail.com>
2018-05-10 22:29:04 +0800

    Revert "session"
    This reverts commit cdd59e640d01794941658e5e9de94758afeb9339.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,7 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'a string of your choice', resave: false, saveUninitialized: true }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
